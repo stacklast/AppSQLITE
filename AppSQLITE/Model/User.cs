d36d739c2549d7f0d1fe732f8fea36ecb928e620
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-01-13 19:51:19 -0500

    feat: Add User Model

---
+++ AppSQLITE/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+namespace AppSQLITE.Model
+{
+    [Table("user")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(20), Unique]
+        public string UserName { get; set; }
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [MaxLength(16)]
+        public string Password { get; set; }
+    }
+}
