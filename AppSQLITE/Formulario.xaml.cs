dd7b804175b7e0236501f7989f1a3fee2dce190e
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-01-13 20:29:42 -0500

    feat: Add form

---
+++ AppSQLITE/Formulario.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppSQLITE
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Formulario : ContentPage
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void btncrear_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlistar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
