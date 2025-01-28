2613f4c7d072e7132586630c52943e6a678467f7
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-01-27 22:43:42 -0500

    docs: Update Readme

--- AppSQLITE.iOS/FileAccess.cs
+++ AppSQLITE.iOS/FileAccess.cs
@@ -1,16 +1,13 @@
-using System.Security.AccessControl;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AppSQLITE.iOS
 {
     public class FileAccess
     {
-        public static String GetLocalFilePath(string filename) {
+        public static String GetLocalFilePath(string filename)
+        {
 
-            string path=System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
 
             return System.IO.Path.Combine(path, filename);
         }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # AppSQLITE
 
-Example Xamarin Forms with SQLite
+Practice Xamarin Forms with SQLite
+
+- Repository Pattern with SQLite connection **UserRepository**
+- Listview with Grid
+- Frame as **modalOverlay**
+- **FileAccess** to get the SQLite path
